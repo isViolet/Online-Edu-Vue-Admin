1125a0dd54726b2b98578f38daca8a597b1d7d75
isViolet <1976334188@qq.com>
2022-09-14 16:33:41 +0800

    修改

--- src/api/edu/course.js
+++ src/api/edu/course.js
@@ -1,10 +1,26 @@
 import request from '@/utils/request'
+
+const api_name = '/edu-service/edu-course'
+
 export default {
   // 1 添加课程信息
   addCourseInfo(courseInfo) {
     return request({
-      url: '/edu-service/edu-course/saveCourseInfo',
+      url: `${api_name}/saveCourseInfo`,
       method: 'post',
+      data: courseInfo
+    })
+  },
+  getCourseInfoById(id) {
+    return request({
+      url: `${api_name}/getById?=${id}`,
+      method: 'get'
+    })
+  },
+  updateCourseInfoById(courseInfo) {
+    return request({
+      url: `${api_name}/updateCourseInfoById?id=${courseInfo.id}`,
+      method: 'put',
       data: courseInfo
     })
   }
--- src/views/edu/course/info.vue
+++ src/views/edu/course/info.vue
@@ -118,12 +118,45 @@ export default {
     }
   },
   created() {
-    // 初始化所有讲师
-    this.getListTeacher()
-    // 初始化一级分类
-    this.getOneSubject()
+    // 获取路由id值
+    if (this.$route.params && this.$route.params.id) {
+      this.courseId = this.$route.params.id
+      // 调用根据id查询课程的方法
+      this.getInfo()
+    } else {
+      // 初始化所有讲师
+      this.getListTeacher()
+      // 初始化一级分类
+      this.getOneSubject()
+    }
   },
   methods: {
+    // 根据课程id查询
+    getInfo() {
+      course.getCourseInfoId(this.courseId)
+        .then(response => {
+          // 在courseInfo课程基本信息，包含 一级分类id 和 二级分类id
+          this.courseInfo = response.data.courseInfoVo
+          // 1 查询所有的分类，包含一级和二级
+          subject.getSubjectList()
+            .then(response => {
+              // 2 获取所有一级分类
+              this.subjectOneList = response.data.list
+              // 3 把所有的一级分类数组进行遍历，
+              for (var i = 0; i < this.subjectOneList.length; i++) {
+                // 获取每个一级分类
+                var oneSubject = this.subjectOneList[i]
+                // 比较当前courseInfo里面一级分类id和所有的一级分类id
+                if (this.courseInfo.subjectParentId == oneSubject.id) {
+                  // 获取一级分类所有的二级分类
+                  this.subjectTwoList = oneSubject.children
+                }
+              }
+            })
+          // 初始化所有讲师
+          this.getListTeacher()
+        })
+    },
     // 上传封面成功调用的方法
     handleAvatarSuccess(res, file) {
       this.courseInfo.cover = res.data.url
@@ -183,6 +216,7 @@ export default {
           this.$router.push({ path: '/course/chapter/' + response.data.courseId })
         })
     }
+
   }
 }
 </script>
